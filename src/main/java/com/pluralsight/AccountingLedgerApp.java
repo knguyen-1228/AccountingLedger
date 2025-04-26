67183c17f6e17d8650adf98e09e74ed3bf653d92
Kevin Nguyen <knguyen@appdev.yearup.org>
2025-04-26 15:53:53 -0500

    create home screen

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -1,7 +1,74 @@
 package com.pluralsight;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
 public class AccountingLedgerApp {
+    static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+        boolean appRunning = true;
+        while(appRunning){
+            String mainMenuChoice = homeScreen().toLowerCase();
+            switch(mainMenuChoice){
+                case "d":
+                    addDeposit();
+                    break;
+                case "p":
+
+                    break;
+                case "l":
+
+                    break;
+                case "x":
+                    System.out.println("Goodbye, See you again!");
+                    appRunning = false;
+                    break;
+
+            }
+        }
+
     }
+    public static String homeScreen(){
+
+        String choice = "";
+
+        while(true) {
+            System.out.println("-------------------------------------");
+            System.out.println("-------------Home Screen-------------");
+            System.out.println("-------------------------------------");
+            System.out.println("[D] Add Deposit\n[P] Make Payment(Debit)\n[L] Ledger\n[X] Exit");
+            System.out.println("What would you like to do?");
+            choice = myScanner.nextLine();
+            break;
+        }
+        return choice;
+    }
+    public static BufferedWriter fileWriter(){
+        try{
+            FileWriter fileWriter = new FileWriter("src/main/resources/transaction.csv",true);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+            bufWriter.write("Date | Time | Description| Vendor | Amount\n");
+            bufWriter.close();
+            return new BufferedWriter(fileWriter);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static BufferedWriter addDeposit(){
+        System.out.println("Enter the description of the deposit:");
+        String description = myScanner.nextLine();
+        System.out.println("Enter the vendor of the deposit:");
+        String vendor = myScanner.nextLine();
+        System.out.println("Enter the amount of the deposit:");
+        double amount = myScanner.nextDouble();
+        Transaction transactions = new Transaction();
+        String listOfDeposit = transactions.getDate() +" | " + transactions.getTime() +" | " + transactions.getDescription() +" | " + transactions.getVendor() +" | " +transactions.getAmount();
+
+        return new BufferedWriter(fileWriter());
+    }
+
+
 }
---
+++ src/main/java/com/pluralsight/Transaction.java
@@ -0,0 +1,60 @@
+package com.pluralsight;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Transaction {
+    private LocalDate date;
+    private LocalDateTime time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    public Transaction(LocalDate date, LocalDateTime time, String description, String vendor, double amount){
+        this.date = date;
+        this.time = time;
+        this.description = description;
+        this.vendor = vendor;
+        this.amount = amount;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public void setVendor(String vendor) {
+        this.vendor = vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
